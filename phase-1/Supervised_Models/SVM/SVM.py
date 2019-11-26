50c0bed6ca6040f280c336389586c97f4d8a07e2
BarbaraDiaz <debi_1223@hotmail.com>
2019-11-26 13:32:58 -0600

    modify tabs SVM.py

--- phase-1/Supervised_Models/SVM/SVM.py
+++ phase-1/Supervised_Models/SVM/SVM.py
@@ -12,7 +12,7 @@ from Functions_SVM import get_atributes, test_compound, test_compound_real_categ
 class SVM:
     
     def __init__(self, root, input_file, target, descriptors, fraction):
-    	self.Data = pd.read_csv(str(root["root"]) + str(input_file)) 
+        self.Data = pd.read_csv(str(root["root"]) + str(input_file)) 
         self.fraction = fraction
         print(self.Data.PPI.unique())
         print("Libraries are: ", self.Data.Library.unique())
@@ -22,14 +22,14 @@ class SVM:
         self.root = root
         
     def train_model(self, kernel, class_weight):
-    	"""
+        """
         kernel: str, ‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’
         class_weight : ‘balanced’
         """
         y = np.array(self.Data[self.target])
         y = label_binarize(y, classes = ["No", "Yes"])
-       	y = np.reshape(y,int(y.shape[0]))
-	X_train, X_test, y_train, y_test = train_test_split(self.Data[self.descriptors], y, test_size = self.fraction, random_state=1992)
+       	y = np.reshape(y, int(y.shape[0]))
+        X_train, X_test, y_train, y_test = train_test_split(self.Data[self.descriptors], y, test_size = self.fraction, random_state=1992)
         model = SVC(kernel = kernel, probability=True, cache_size = 16,  class_weight =  class_weight, random_state=1992)
         print(model)
         model.fit(X_train, y_train)
@@ -54,4 +54,3 @@ class SVM:
     def report(self, ref_output):
         roc_auc = plot_roc(ref_output,self.predictions["y_test"], self.predictions["y_score"], self.root["root_ROC"])
         svm_report(ref_output, self.Data, self.parameters, self.predictions["y_test"], self.predictions["predictions"], self.descriptors, self.atributes,roc_auc, self.root["root_Info"])
-        
