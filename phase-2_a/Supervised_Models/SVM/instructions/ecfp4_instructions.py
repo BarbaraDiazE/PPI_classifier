fc7ce5f50479b450d4f1eaead716b8f7b6808e58
Barbara <debi_1223@hotmail.com>
2021-01-15 17:59:50 -0600

    results ecfp4 models

--- phase-2_a/Supervised_Models/SVM/instructions/ecfp4_instructions.py
+++ phase-2_a/Supervised_Models/SVM/instructions/ecfp4_instructions.py
@@ -1,122 +1,111 @@
 import pandas as pd
 from SVM_FP import SVM
 
-# root = {"root": "/tmpu/jlmf_g/jlmf/B/PPI_classifier/phase-1/Databases/",
-#        "root_Info" : "/tmpu/jlmf_g/jlmf/B/PPI_classifier/phase-1/Supervised_Models/SVM/Info",
-#        "root_ROC" : "/tmpu/jlmf_g/jlmf/B/PPI_classifier/phase-1/Supervised_Models/SVM/ROC"}
+print("file ecfp4")
 
-root = {
-    "root": "/home/babs/Documents/DIFACQUIM/PPI_classifier/phase-2_a/Databases/",
-    "root_Info": "/home/babs/Documents/DIFACQUIM/PPI_classifier/phase-2_a/Supervised_Models/SVM/Info",
-    "root_ROC": "/home/babs/Documents/DIFACQUIM/PPI_classifier/phase-2_a/Supervised_Models/SVM/ROC",
-    "trained_models": "/home/babs/Documents/DIFACQUIM/PPI_classifier/phase-2_a/trained_models",
-}
+# root = {
+#     "root": "/home/babs/Documents/DIFACQUIM/PPI_classifier/phase-2_a/Databases/",
+#     "root_Info": "/home/babs/Documents/DIFACQUIM/PPI_classifier/phase-2_a/Supervised_Models/SVM/Info",
+#     "root_ROC": "/home/babs/Documents/DIFACQUIM/PPI_classifier/phase-2_a/Supervised_Models/SVM/ROC",
+#     "trained_models": "/home/babs/Documents/DIFACQUIM/PPI_classifier/phase-2_a/trained_models",
+# }
 
-# descriptors = ['HBA', 'HBD', 'RB', 'LogP', 'TPSA', 'MW', 'Heavy Atom', 'Ring Count', 'Fraction CSP3']
-"""MACCS"""
-print(str(root["root"]))
-Data = pd.read_csv(str(root["root"]) + str("dataset_maccskeys_p2.csv"))
-ids = ["Unnamed: 0", "ipp_id", "chembl_id", "SMILES", "library", "PPI family", "PPI"]
-numerical_data = Data.drop(ids, axis=1)
-descriptors = numerical_data.columns.to_list()
-print(len(descriptors))
+# """ECFP4 models"""
+# print("ecfp4")
+# print(str(root["root"]))
+# Data = pd.read_csv(str(root["root"]) + str("dataset_ecfp4_p2.csv"))
+# ids = ["Unnamed: 0", "ipp_id", "chembl_id", "SMILES", "library", "PPI family", "PPI"]
+# numerical_data = Data.drop(ids, axis=1)
+# descriptors = numerical_data.columns.to_list()
+# print(len(descriptors))
 
 
-def execute(
-    root, input_file, target, descriptors, fraction, kernel, balanced, ref_output
-):
-    a = SVM(root, input_file, target, descriptors, fraction)
-    a.train_model(kernel, balanced)
-    a.report(ref_output)
-    print("report ", str(ref_output), "is done")
+# def execute(
+#     root, input_file, target, descriptors, fraction, kernel, balanced, ref_output
+# ):
+#     a = SVM(root, input_file, target, descriptors, fraction)
+#     a.train_model(kernel, balanced)
+#     a.report(ref_output)
+#     print("report ", str(ref_output), "is done")
 
 
-execute(
-    root,
-    "dataset_maccskeys_p2.csv",
-    "PPI",
-    descriptors,
-    0.2,
-    "linear",
-    "balanced",
-    "p2_F3L6P3SVM1A",
-)
 # execute(
 #     root,
-#     "p2_MACCS_L6.csv",
+#     "dataset_ecfp4_p2.csv",
+#     "PPI",
+#     descriptors,
+#     0.2,
+#     "linear",
+#     None,
+#     "p2_F1L6P3SVM1ABN",
+# )
+# execute(
+#     root,
+#     "dataset_ecfp4_p2.csv",
 #     "PPI",
 #     descriptors,
 #     0.2,
 #     "poly",
-#     "balanced",
-#     "p2_F3L6P3SVM2A",
+#     None,
+#     "p2_F1L6P3SVM2ABN",
 # )
 # execute(
 #     root,
-#     "p2_MACCS_L6.csv",
+#     "dataset_ecfp4_p2.csv",
 #     "PPI",
 #     descriptors,
 #     0.2,
 #     "rbf",
-#     "balanced",
-#     "p2_F3L6P3SVM3A",
+#     None,
+#     "p2_F1L6P3SVM3ABN",
 # )
 # execute(
 #     root,
-#     "p2_MACCS_L6.csv",
+#     "dataset_ecfp4_p2.csv",
 #     "PPI",
 #     descriptors,
 #     0.2,
 #     "sigmoid",
-#     "balanced",
-#     "p2_F3L6P3SVM4A",
+#     None,
+#     "p2_F1L6P3SVM4ABN",
 # )
 # execute(
 #     root,
-#     "p2_MACCS_L6.csv",
+#     "dataset_ecfp4_p2.csv",
 #     "PPI",
 #     descriptors,
 #     0.3,
 #     "linear",
-#     "balanced",
-#     "p2_F3L6P5SVM1A",
+#     None,
+#     "p2_F1L6P5SVM1ABN",
 # )
 # execute(
 #     root,
-#     "p2_MACCS_L6.csv",
+#     "dataset_ecfp4_p2.csv",
 #     "PPI",
 #     descriptors,
 #     0.3,
 #     "poly",
-#     "balanced",
-#     "p2_F3L6P5SVM2A",
+#     None,
+#     "p2_F1L6P5SVM2ABN",
 # )
 # execute(
 #     root,
-#     "p2_MACCS_L6.csv",
+#     "dataset_ecfp4_p2.csv",
 #     "PPI",
 #     descriptors,
 #     0.3,
 #     "rbf",
-#     "balanced",
-#     "p2_F3L6P5SVM3A",
+#     None,
+#     "p2_F1L6P5SVM3ABN",
 # )
 # execute(
 #     root,
-#     "p2_MACCS_L6.csv",
+#     "dataset_ecfp4_p2.csv",
 #     "PPI",
 #     descriptors,
 #     0.3,
 #     "sigmoid",
-#     "balanced",
-#     "p2_F3L6P5SVM4A",
+#     None,
+#     "p2_F1L6P5SVM4ABN",
 # )
-
-# execute(root, "ECFP6_L6.csv", "PPI", descriptors, 0.2, "linear", None, "F2L6P3SVM1ABN")
-# execute(root, "ECFP6_L6.csv", "PPI", descriptors, 0.2, "poly", None, "F2L6P3SVM2ABN")
-# execute(root, "ECFP6_L6.csv", "PPI", descriptors, 0.2, "rbf", None, "F2L6P3SVM3ABN")
-# execute(root, "ECFP6_L6.csv", "PPI", descriptors, 0.2, "sigmoid", None, "F2L6P3SVM4ABN")
-# execute(root, "ECFP6_L6.csv", "PPI", descriptors, 0.3, "linear", None, "F2L6P5SVM1ABN")
-# # execute(root, "ECFP6_L6.csv", "PPI", descriptors, 0.3, "poly", None, "F2L6P5SVM2ABN")
-# execute(root, "ECFP6_L6.csv", "PPI", descriptors, 0.3, "rbf", None, "F2L6P5SVM3ABN")
-# execute(root, "ECFP6_L6.csv", "PPI", descriptors, 0.3, "sigmoid", None, "F2L6P5SVM4ABN")
