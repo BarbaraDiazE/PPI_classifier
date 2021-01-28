95ff0cd0cf058d76c1dbfdf8684b347d66a61a97
Barbara <debi_1223@hotmail.com>
2021-01-28 14:58:40 -0600

    update plot_precision.py

--- phase-2_a/Supervised_Models/SVM/Info/plot_precision.py
+++ phase-2_a/Supervised_Models/SVM/Info/plot_precision.py
@@ -19,6 +19,10 @@ for file in os.listdir():
         arr.append(file)
 DF = pd.DataFrame
 
+# read id models csv
+id_dataframe = pd.read_csv(f'{root["SVM"]}{"p2_id_models.csv"}', index_col="Unnamed: 0")
+print(id_dataframe.head())
+
 
 def storage_info(arr):
     """storage Precision"""
@@ -39,20 +43,39 @@ def storage_info(arr):
     return DF
 
 
+def rename_x(model_name):
+    model_name = model_name.replace(".csv", "")
+    _ = id_dataframe.loc[id_dataframe["model name"] == model_name, "id_model"]
+    return _.iloc[0]
+
+
+def unify_string_len(id_model):
+    if len(id_model) == 3:
+        label = id_model + "  "
+    else:
+        label = id_model
+    return label
+
+
 def plot_sim(DF):
     plt.figure(figsize=[10, 4.8], dpi=200)
     DF.to_csv(
         "/home/babs/Documents/DIFACQUIM/PPI_classifier/phase-2_a/Supervised_Models/SVM/Info/info_metrics/Precision.csv"
     )
     DF = DF.sort_values(by=["Exp"])
-    X = list()
-    for i in DF.Exp.to_list():
-        i = i.replace(".csv", "")
-        X.append(i.replace("SVM_", ""))
+    # X = list()
+    # for i in DF.Exp.to_list():
+    #     X.append(i)
+
     x = [i for i in range(len(DF["Precision"]))]
+    # xlabels
+    X = DF.Exp.to_list()
+    X = list(map(rename_x, X))
+    X_label = list(map(unify_string_len, X))
+    print("soy x", "\n", X_label)
     y = list(DF["Precision"])
-    plt.plot(x, y, "ro", color="cyan")
-    plt.xticks(x, X, rotation="vertical")
+    plt.plot(x, y, "ro", color="teal")
+    plt.xticks(x, X_label, rotation="vertical")
     # plt.tick_params(axis="x", labelsize=10)
     plt.ylabel("Precision")
     plt.subplots_adjust(bottom=0.35)
